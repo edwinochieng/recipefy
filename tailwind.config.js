c831899cc766645036e9218500193e4f9713a3d0
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-23 23:27:17 +0300

    add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        "poppins-medium": ["PoppinsMedium"],
+        "poppins-bold": ["PoppinsBold"],
+      },
+    },
   },
   plugins: [],
 };
