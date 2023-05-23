621b029da6777e0f9db69c356c85c054a87ec4db
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-23 23:40:42 +0300

    home page

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import { Text } from "react-native";
 
 export default function Home() {
-  return <Text className='text-5xl text-red-500'>This is my Home page</Text>;
+  return (
+    <Text className='text-5xl text-red-500 font-poppins'>
+      This is my Home page
+    </Text>
+  );
 }
