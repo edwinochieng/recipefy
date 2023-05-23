1270431b2b2860a3909af22da770fe18b20d09e9
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-23 23:29:00 +0300

    load fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Poppins: require("../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
