e65f50d9a3532af5106d1de0c6b4194f4301dbbd
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-23 19:27:07 +0300

    rename home route

--- app/home.js
+++
@@ -1,5 +0,0 @@
-import { Text } from "react-native";
-
-export default function Home() {
-  return <Text className='text-bold text-4xl'>Home Page</Text>;
-}
---
+++ app/index.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Home() {
+  return <Text className='text-5xl text-red-500'>This is my Home page</Text>;
+}
