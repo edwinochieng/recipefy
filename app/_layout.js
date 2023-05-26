9b25660646e8ee4b9a2ae7e863b8eb52c4a37d16
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-26 21:55:06 +0300

    remove header titles

--- app/_layout.js
+++ app/_layout.js
@@ -12,5 +12,11 @@ export default function Layout() {
     return null;
   }
 
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerTitle: "",
+      }}
+    />
+  );
 }
