548b8aa337e4315e361ad112262cdbcc0885c6cc
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-26 22:43:57 +0300

    add route to signup page

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
-import { Text } from "react-native";
+import { useRouter } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <Text className='text-5xl text-red-500 font-poppins'>
-      This is my Home page
+    <Text className='text-lg text-red-500 font-poppins'>
+      <Pressable onPress={() => router.push("auth/signup")}>
+        <Text>Sign Up</Text>
+      </Pressable>
     </Text>
   );
 }
