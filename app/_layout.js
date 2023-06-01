f2d7bb7bb178824afe6542b9ed95fd56862f2297
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-01 17:46:49 +0300

    hide auth group stack navigators

--- app/_layout.js
+++ app/_layout.js
@@ -15,6 +15,7 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
     </Stack>
   );
 }
