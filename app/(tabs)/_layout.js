a9e2917061c6a808960724e4986b27217cce4e27
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-01 16:42:48 +0300

    add home, search and profile tabs

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,38 @@
+import { Tabs } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Octicons } from "@expo/vector-icons";
+
+export default function Layout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='home'
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name='home' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='search'
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='search-outline' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name='account' size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
