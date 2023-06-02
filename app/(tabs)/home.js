a0de5778803cd68cdd91ba5422c1c8eb11f52e33
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-03 00:46:13 +0300

    add scrolling

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -1,13 +1,18 @@
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import React from "react";
 import Search from "../../components/Search";
 import Categories from "../../components/Categories";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Home() {
   return (
-    <SafeAreaView className='flex-1 '>
-      <Search />
-      <Categories />
+    <SafeAreaView className='flex-1'>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className='flex-1 px-4'>
+          <Search />
+          <Categories />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
