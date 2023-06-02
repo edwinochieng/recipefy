f1fe66b5ec37f1bd8366664d5541d4144185cda5
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-02 13:36:53 +0300

    style search component

--- components/Search.jsx
+++ components/Search.jsx
@@ -6,21 +6,21 @@ export default function Search() {
   const [query, setQuery] = useState();
 
   return (
-    <View className='flex-row'>
-      <View className='flex-row'>
-        <View className='absolute left-0'>
-          <Ionicons name='search' size={24} color='black' />
+    <View className='flex-row space-x-2 mt-5'>
+      <View className='flex-row items-center bg-gray-200 rounded-xl w-4/5'>
+        <View className='ml-1'>
+          <Ionicons name='search' size={22} color='gray' />
         </View>
 
         <TextInput
           placeholder='Search by food name'
           value={query}
           onChangeText={(text) => setQuery(text)}
-          className='p-2 rounded-md bg-gray-200'
+          className='p-2 bg-transparent font-poppins w-full'
         />
       </View>
 
-      <View className='bg-gray-200 rounded p-2 items-center justify-center'>
+      <View className='bg-gray-200 rounded-lg p-2 items-center justify-center  '>
         <Ionicons name='ios-filter' size={24} color='black' />
       </View>
     </View>
