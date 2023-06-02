aa2bf1789b20aee54b07a07914510b0d7080c711
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-02 23:31:35 +0300

    add welcome message

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,4 +1,4 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, Text } from "react-native";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -6,22 +6,32 @@ export default function Search() {
   const [query, setQuery] = useState();
 
   return (
-    <View className='flex-row space-x-2 mt-5'>
-      <View className='flex-row items-center bg-gray-200 rounded-xl w-4/5'>
-        <View className='ml-1'>
-          <Ionicons name='search' size={22} color='gray' />
+    <View className='absolute left-4 top-0 right-4'>
+      <View className='mt-2'>
+        <Text className='text-gray-500 font-poppins-medium text-base mb-1'>
+          Hello Edwin
+        </Text>
+        <Text className='text-3xl font-poppins-bold text-gray-700 '>
+          What would you like {"\n"} to cook today?
+        </Text>
+      </View>
+      <View className='flex-row justify-between space-x-2 mt-3'>
+        <View className='flex-row items-center bg-gray-200 rounded-xl flex-1 '>
+          <View className='ml-1'>
+            <Ionicons name='search' size={22} color='gray' />
+          </View>
+
+          <TextInput
+            placeholder='Search by food name'
+            value={query}
+            onChangeText={(text) => setQuery(text)}
+            className='p-2 bg-transparent font-poppins flex-1 rounded-xl'
+          />
         </View>
 
-        <TextInput
-          placeholder='Search by food name'
-          value={query}
-          onChangeText={(text) => setQuery(text)}
-          className='p-2 bg-transparent font-poppins w-full'
-        />
-      </View>
-
-      <View className='bg-gray-200 rounded-lg p-2 items-center justify-center  '>
-        <Ionicons name='ios-filter' size={24} color='black' />
+        <View className='bg-gray-200 rounded-lg px-2 items-center justify-center  '>
+          <Ionicons name='ios-filter' size={24} color='black' />
+        </View>
       </View>
     </View>
   );
