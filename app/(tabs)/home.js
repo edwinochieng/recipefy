a732190742c4b1c2d7f59db8505143513c687a1a
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-03 13:22:23 +0300

    set screen height

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -2,13 +2,12 @@ import { SafeAreaView, ScrollView, View } from "react-native";
 import React from "react";
 import Search from "../../components/Search";
 import Categories from "../../components/Categories";
-import { ScrollView } from "react-native-gesture-handler";
 
 export default function Home() {
   return (
     <SafeAreaView className='flex-1'>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View className='flex-1 px-4'>
+        <View className='flex-1 p-4 min-h-screen'>
           <Search />
           <Categories />
         </View>
