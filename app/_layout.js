d6ddd98755c36c228876c24e150960d89bb3f09e
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-04 17:54:31 +0300

    wrap QueryClient provider

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -13,9 +16,11 @@ export default function Layout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }
