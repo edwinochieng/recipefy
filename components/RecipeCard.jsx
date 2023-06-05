5331c334341640bd96c5247cc7b4701c8e4a8cb0
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-05 23:15:23 +0300

    add image and title

--- components/RecipeCard.jsx
+++ components/RecipeCard.jsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
-export default function RecipeCard() {
+export default function RecipeCard({ recipe }) {
+  const imageUri = recipe.image;
   return (
-    <View>
-      <Text>RecipeCard</Text>
+    <View className='bg-white rounded-lg'>
+      <View>
+        <Image
+          source={{ uri: imageUri }}
+          className='flex-1 h-[200px] rounded-lg'
+        />
+      </View>
+      <View className='flex-1 mt-1 px-2'>
+        <Text className='font-poppins-medium text-gray-700'>
+          {recipe.title}
+        </Text>
+      </View>
     </View>
   );
 }
