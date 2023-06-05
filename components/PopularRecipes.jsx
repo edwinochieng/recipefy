8c8e6b49f11f2f90cba349bd925f62ae037bfe78
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-05 23:16:03 +0300

    add grid

--- components/PopularRecipes.jsx
+++ components/PopularRecipes.jsx
@@ -3,28 +3,36 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { SPOONACULAR_API_KEY } from "@env";
+import RecipeCard from "./RecipeCard";
 
 const url = "https://api.spoonacular.com/recipes/random";
 
 export default function PopularRecipes() {
   const fetchPopularRecipes = async () => {
     const res = await axios.get(
-      `${url}?apiKey=${SPOONACULAR_API_KEY}&number=10`
+      `${url}?apiKey=${SPOONACULAR_API_KEY}&number=8`
     );
 
     return res.data;
   };
 
-  const query = useQuery({
-    queryKey: ["recipes"],
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["popularRecipes"],
     queryFn: fetchPopularRecipes,
   });
 
   return (
-    <View className='px-4 mt-6 flex-1'>
+    <View className='px-4 my-6 flex-1'>
       <Text className='font-poppins-bold text-lg text-gray-700 '>
         Popular Recipes
       </Text>
+      <View className='flex-row flex-wrap justify-between mt-1'>
+        {data?.recipes.map((recipe) => (
+          <View key={recipe.id} className='w-[49%] my-1'>
+            <RecipeCard recipe={recipe} />
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
