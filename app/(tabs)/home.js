5db8ab839736ce50aa33b9ac372fdc12121eeaa1
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-07 18:11:54 +0300

    add welcome message component

--- app/(tabs)/home.js
+++ app/(tabs)/home.js
@@ -4,12 +4,14 @@ import Search from "../../components/Search";
 import Categories from "../../components/Categories";
 import Recommendation from "../../components/Recommendation";
 import PopularRecipes from "../../components/PopularRecipes";
+import Welcome from "../../components/Welcome";
 
 export default function Home() {
   return (
     <SafeAreaView className='flex-1'>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View className='flex-1'>
+          <Welcome />
           <Search />
           <Categories />
           <Recommendation />
