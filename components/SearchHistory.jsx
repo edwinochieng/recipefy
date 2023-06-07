de153599ac7c3ba9d5465d6bd6609ad01155526d
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-07 23:02:07 +0300

    display search history

---
+++ components/SearchHistory.jsx
@@ -0,0 +1,38 @@
+import { View, Text, FlatList, Pressable } from "react-native";
+import React from "react";
+import useStore from "../store/store";
+
+export default function SearchHistory() {
+  const searchHistory = useStore((state) => state.searchHistory);
+  const removeSearch = useStore((state) => state.removeSearch);
+  return (
+    <View className='flex-1 px-4'>
+      {searchHistory.length > 0 ? (
+        <View className='mt-3'>
+          <View className='flex-row justify-between'>
+            <Text className='font-semibold'>Recent Searches</Text>
+            <Text className='font-poppins-medium text-xs'>Clear history</Text>
+          </View>
+
+          <View className='my-1'>
+            <FlatList
+              data={searchHistory}
+              renderItem={({ item }) => (
+                <View className='bg-white rounded-md p-2 flex-row justify-between'>
+                  <Text>{item.title}</Text>
+                  <Pressable onPress={() => removeSearch()}>
+                    <Text>Remove</Text>
+                  </Pressable>
+                </View>
+              )}
+            />
+          </View>
+        </View>
+      ) : (
+        <View className='flex-1 justify-center item-center'>
+          <Text className='text-2xl'>Search recipes</Text>
+        </View>
+      )}
+    </View>
+  );
+}
