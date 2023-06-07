7f0d1ff863449560e4edce26b935f99d424e0db1
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-07 12:00:33 +0300

    fetch and dsplay recipe based on category

--- app/categories/[slug].js
+++ app/categories/[slug].js
@@ -1,12 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import { useQuery } from "@tanstack/react-query";
+import { SPOONACULAR_API_KEY } from "@env";
+import RecipeCard from "../../components/RecipeCard";
+import axios from "axios";
+
+const url = "https://api.spoonacular.com/recipes/random";
 
 export default function Categories() {
   const { slug } = useLocalSearchParams();
+
+  const fetchRecipes = async () => {
+    const res = await axios.get(
+      `${url}?apiKey=${SPOONACULAR_API_KEY}&number=20&tags=${slug}`
+    );
+
+    return res.data;
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryFn: fetchRecipes,
+    queryKey: ["categorizedRecipes"],
+  });
+
   return (
-    <View>
-      <Text>{slug}</Text>
-    </View>
+    <SafeAreaView className='flex-1'>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className='flex-1 p-4'>
+          <View className='flex-row flex-wrap justify-between mt-1'>
+            {data?.recipes.map((recipe) => (
+              <View key={recipe.id} className='w-[49%] my-1'>
+                <RecipeCard recipe={recipe} />
+              </View>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
