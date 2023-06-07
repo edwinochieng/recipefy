cc9c5183a28fa2f021b31b923de85e7551e6f581
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-07 23:02:35 +0300

    add search and search history components

--- app/(tabs)/search.js
+++ app/(tabs)/search.js
@@ -1,10 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import Search from "../../components/Search";
+import SearchHistory from "../../components/SearchHistory";
 
-export default function Search() {
+export default function SearchScreen() {
   return (
-    <View>
-      <Text>Search page</Text>
-    </View>
+    <SafeAreaView className='flex-1'>
+      <View className='flex-1'>
+        <Search />
+        <SearchHistory />
+      </View>
+    </SafeAreaView>
   );
 }
