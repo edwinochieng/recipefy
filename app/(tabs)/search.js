d84990ac955209195aa5edd47c9cbc6a02e3bafb
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-08 22:52:36 +0300

    fetch recipe data

--- app/(tabs)/search.js
+++ app/(tabs)/search.js
@@ -1,15 +1,45 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import Search from "../../components/Search";
 import SearchHistory from "../../components/SearchHistory";
+import axios from "axios";
+import { useLocalSearchParams } from "expo-router";
+import SearchResults from "../../components/SearchResults";
+import { useQuery } from "@tanstack/react-query";
+import { SPOONACULAR_API_KEY } from "@env";
+
+const url = "https://api.spoonacular.com/recipes/complexSearch";
 
 export default function SearchScreen() {
+  const { query } = useLocalSearchParams();
+
+  const searchQuery = query;
+
+  const fetchRecipes = async () => {
+    const res = await axios.get(
+      `${url}apiKey=${SPOONACULAR_API_KEY}&query=${searchQuery}`
+    );
+
+    return res.data;
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["searchResults"],
+    queryFn: fetchRecipes,
+  });
+
+  const results = data?.results;
+
   return (
     <SafeAreaView className='flex-1'>
-      <View className='flex-1'>
-        <Search />
-        <SearchHistory />
-      </View>
+      {results ? (
+        <SearchResults results={results} />
+      ) : (
+        <View className='flex-1'>
+          <Search />
+          <SearchHistory />
+        </View>
+      )}
     </SafeAreaView>
   );
 }
