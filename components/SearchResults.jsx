13f1e5a79f1e69536776e975e2aeb68ea0085b73
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-10 20:51:15 +0300

    refactor code

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -3,28 +3,31 @@ import React from "react";
 import RecipeCard from "./RecipeCard";
 
 export default function SearchResults({ results }) {
+  if (results.length === 0) {
+    return (
+      <View className='flex-1 justify-center items-center'>
+        <Text>Oppss!!No results found</Text>
+      </View>
+    );
+  }
+
   return (
-    <>
-      {results?.length > 0 ? (
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View className='px-4 my-6 flex-1'>
-            <Text className='font-poppins-bold text-lg text-gray-700 '>
-              Search results
-            </Text>
-            <View className='flex-1 flex-row flex-wrap justify-between mt-1'>
-              {results.map((recipe) => (
-                <View key={recipe.id} className='w-[49%] my-1'>
-                  <RecipeCard recipe={recipe} />
-                </View>
-              ))}
-            </View>
-          </View>
-        </ScrollView>
-      ) : (
-        <View className='flex-1 justify-center items-center'>
-          <Text>Oppss!!No results found</Text>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View className='flex-1'>
+        <View className='my-1'>
+          <Text className='font-poppins-bold text-lg text-gray-700 '>
+            Search results
+          </Text>
         </View>
-      )}
-    </>
+
+        <View className='flex flex-row flex-wrap justify-between mt-1'>
+          {results.map((recipe) => (
+            <View key={recipe.id} className='w-[49%] my-1'>
+              <RecipeCard recipe={recipe} />
+            </View>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
   );
 }
