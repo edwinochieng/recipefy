4a9f16af9a77bb80bc2de28885ca25b5ea25f367
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-11 19:43:43 +0300

    display saved recipes

--- app/(tabs)/saved.js
+++ app/(tabs)/saved.js
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { View, SafeAreaView, ScrollView, Text } from "react-native";
 import React from "react";
+import RecipeCard from "../../components/RecipeCard";
+import { useFavoriteRecipesStore } from "../../store/store";
 
 export default function Saved() {
+  const favoriteRecipes = useFavoriteRecipesStore(
+    (state) => state.favoriteRecipes
+  );
+
   return (
-    <View>
-      <Text>Saved</Text>
-    </View>
+    <SafeAreaView className='flex-1'>
+      {favoriteRecipes?.length > 0 ? (
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View className='flex-1 p-4'>
+            <View className='flex-row flex-wrap justify-between mt-1'>
+              {favoriteRecipes?.map((recipe) => (
+                <View key={recipe.id} className='w-[49%] my-1'>
+                  <RecipeCard recipe={recipe} />
+                </View>
+              ))}
+            </View>
+          </View>
+        </ScrollView>
+      ) : (
+        <View className='flex-1 items-center justify-center'>
+          <Text>Opps!No saved recipes!</Text>
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
