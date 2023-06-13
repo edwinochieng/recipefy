ab18bd9aaa0fe3edf5fb99a3ff9a873becfefd91
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-13 23:30:53 +0300

    add filter data

---
+++ assets/filter.js
@@ -0,0 +1,102 @@
+import categories from "./categories";
+
+const cusines = [
+  {
+    name: "African",
+  },
+  {
+    name: "Asian",
+  },
+  {
+    name: "Caribbean",
+  },
+  {
+    name: "Chinese",
+  },
+  {
+    name: "French",
+  },
+  {
+    name: "Greek",
+  },
+  {
+    name: "Indian",
+  },
+  {
+    name: "Italian",
+  },
+  {
+    name: "Japanese",
+  },
+  {
+    name: "Korean",
+  },
+  {
+    name: "Mexican",
+  },
+  {
+    name: "Spanish",
+  },
+  {
+    name: "Thai",
+  },
+];
+
+const mealTypes = categories;
+
+const diet = [
+  {
+    name: "Vegetarian",
+  },
+  {
+    name: "Gluten Free",
+  },
+  {
+    name: "Lacto-Vegetarian",
+  },
+  {
+    name: "Vegan",
+  },
+  {
+    name: "Primal",
+  },
+  {
+    name: "Paleo",
+  },
+];
+
+const itolerances = [
+  {
+    name: "Dairy",
+  },
+  {
+    name: "Egg",
+  },
+  {
+    name: "Gluten",
+  },
+  {
+    name: "Peanut",
+  },
+  {
+    name: "Seafood",
+  },
+  {
+    name: "Sesame",
+  },
+  {
+    name: "Soy",
+  },
+  {
+    name: "Wheat",
+  },
+];
+
+const maxPrepationTime = [
+  { time: 15 },
+  { time: 30 },
+  { time: 45 },
+  { time: 60 },
+];
+
+export { cusines, mealTypes, diet, itolerances, maxPrepationTime };
