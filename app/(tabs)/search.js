9abd9f4027d2a6f146a040216bc22d7ebbdca563
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-17 00:28:59 +0300

    update fetch function to accomodate search filters

--- app/(tabs)/search.js
+++ app/(tabs)/search.js
@@ -11,11 +11,39 @@ import { SPOONACULAR_API_KEY } from "@env";
 const url = "https://api.spoonacular.com/recipes/complexSearch";
 
 export default function SearchScreen() {
-  const { query } = useLocalSearchParams();
+  const { query, cuisine, diet, type, intolerances, maxReadyTime } =
+    useLocalSearchParams();
+  const queryParams = [];
+
+  if (query) {
+    queryParams.push(`query=${query}`);
+  }
+
+  if (diet) {
+    queryParams.push(`diet=${diet}`);
+  }
+  if (type) {
+    queryParams.push(`type=${type}`);
+  }
+
+  if (cuisine) {
+    queryParams.push(`cuisine=${cuisine}`);
+  }
+  if (intolerances) {
+    queryParams.push(`intolerances=${intolerances}`);
+  }
+
+  if (maxReadyTime) {
+    queryParams.push(`maxReadyTime=${maxReadyTime}`);
+  }
+
+  const queryString = queryParams.join("&");
 
   const fetchRecipes = async () => {
     const res = await axios.get(
-      `${url}?apiKey=${SPOONACULAR_API_KEY}&query=${query}`
+      `${url}?apiKey=${SPOONACULAR_API_KEY}${
+        queryString ? `&${queryString}` : ""
+      }`
     );
 
     return res.data;
