78ab389c7149734ae765f08fe5f396c04ec09ddf
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-06-17 18:31:31 +0300

    check auth state

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,9 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { useFonts } from "expo-font";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BackButton from "../components/BackButton";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +17,16 @@ export default function Layout() {
   if (!fontsLoaded) {
     return null;
   }
+
+  const router = useRouter();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      router.replace("/");
+    } else {
+      router.replace("/login");
+    }
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
