df3c26c0fd209bf351eb4e951b562133be360c89
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-09-08 20:15:31 +0300

    update babel

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,7 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          envName: "APP_ENV",
-          moduleName: "@env",
-          path: ".env",
-        },
-      ],
       "nativewind/babel",
-      "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
     ],
