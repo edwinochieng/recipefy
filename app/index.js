2c76852970716d0f3009da4f2a96e54db1ea73b8
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-09-12 22:24:34 +0300

    refactor code

--- app/index.js
+++ app/index.js
@@ -1,14 +1,5 @@
-import { Redirect, useRouter } from "expo-router";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebaseConfig";
+import { Redirect } from "expo-router";
 
 export default function Home() {
-  const router = useRouter();
-  onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      router.replace("/login");
-    }
-  });
-
   return <Redirect href='/home' />;
 }
