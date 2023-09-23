40146f6cd54dcbcea16506ca48110245ee8779bc
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-09-23 13:34:28 +0300

    wrap auth-session provider

--- app/_layout.js
+++ app/_layout.js
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BackButton from "../components/BackButton";
+import { AuthContextProvider } from "../auth/AuthContext";
 
 const queryClient = new QueryClient();
 
@@ -17,25 +18,27 @@ export default function Layout() {
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack>
-        <Stack.Screen name='(home)' options={{ headerShown: false }} />
-        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
-        <Stack.Screen
-          name='categories/[slug]'
-          options={{
-            headerLeft: () => <BackButton />,
-            headerTitle: "",
-          }}
-        />
-        <Stack.Screen
-          name='details/[id]'
-          options={{
-            headerLeft: () => <BackButton />,
-            headerTitle: "",
-          }}
-        />
-      </Stack>
-    </QueryClientProvider>
+    <AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+          <Stack.Screen name='(home)' options={{ headerShown: false }} />
+          <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+          <Stack.Screen
+            name='categories/[slug]'
+            options={{
+              headerLeft: () => <BackButton />,
+              headerTitle: "",
+            }}
+          />
+          <Stack.Screen
+            name='details/[id]'
+            options={{
+              headerLeft: () => <BackButton />,
+              headerTitle: "",
+            }}
+          />
+        </Stack>
+      </QueryClientProvider>
+    </AuthContextProvider>
   );
 }
