8f5cd03b6d0869e2712e03709d473b326e4edc64
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-09-23 12:42:33 +0300

    fix login error

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -22,9 +22,7 @@ export default function Login() {
     },
   });
 
-  const handleLogin = async ({ email, password }) => {
-    console.log(email);
-    console.log(password);
+  const handleSignIn = async ({ email, password }) => {
     try {
       const userCredential = await signInWithEmailAndPassword(
         auth,
@@ -32,104 +30,103 @@ export default function Login() {
         password
       );
       const user = userCredential.user;
-      console.log("Logged in");
-      router.replace("/");
+      router.push("/");
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
-      console.log(errorMessage);
     }
-    return (
-      <SafeAreaView className='flex-1 bg-white'>
-        <View className='flex-1 p-4 absolute top-[10%]'>
-          <View className='mb-10'>
-            <Text className='text-2xl font-bold text-gray-800'>Log in</Text>
-          </View>
+  };
 
-          <Controller
-            control={control}
-            rules={{
-              required: "Email is required",
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
+  return (
+    <SafeAreaView className='flex-1 bg-white'>
+      <View className='flex-1 p-4 absolute top-[10%]'>
+        <View className='mb-10'>
+          <Text className='text-2xl font-bold text-gray-800'>Log in</Text>
+        </View>
+
+        <Controller
+          control={control}
+          rules={{
+            required: "Email is required",
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder='Email'
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              className={`rounded-lg mt-3 p-2 border ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              } focus:border-blue-500`}
+            />
+          )}
+          name='email'
+        />
+        {errors.email && (
+          <Text className='text-red-500'>{errors.email.message}</Text>
+        )}
+        <Controller
+          control={control}
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters",
+            },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View className='flex flex-row items-center justify-between mt-3'>
               <TextInput
-                placeholder='Email'
+                placeholder='Password'
                 onBlur={onBlur}
+                secureTextEntry={!passwordVisible}
                 onChangeText={onChange}
                 value={value}
-                className={`rounded-lg mt-3 p-2 border ${
-                  errors.email ? "border-red-500" : "border-gray-300"
+                className={`w-full h-full rounded-lg p-2 border ${
+                  errors.password ? "border-red-500" : "border-gray-300"
                 } focus:border-blue-500`}
               />
-            )}
-            name='email'
-          />
-          {errors.email && (
-            <Text className='text-red-500'>{errors.email.message}</Text>
-          )}
-          <Controller
-            control={control}
-            rules={{
-              required: "Password is required",
-              minLength: {
-                value: 6,
-                message: "Password must be at least 6 characters",
-              },
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <View className='flex flex-row items-center justify-between mt-3'>
-                <TextInput
-                  placeholder='Password'
-                  onBlur={onBlur}
-                  secureTextEntry={!passwordVisible}
-                  onChangeText={onChange}
-                  value={value}
-                  className={`w-full h-full rounded-lg p-2 border ${
-                    errors.password ? "border-red-500" : "border-gray-300"
-                  } focus:border-blue-500`}
-                />
-                <Pressable
-                  onPress={() => setPasswordVisible((prevValue) => !prevValue)}
-                  className='absolute right-0 mr-2'
-                >
-                  <Ionicons
-                    name={passwordVisible ? "eye-off-outline" : "eye-outline"}
-                    size={20}
-                    color='gray'
-                  />
-                </Pressable>
-              </View>
-            )}
-            name='password'
-          />
-          {errors.password && (
-            <Text className='text-red-500'>{errors.password.message}</Text>
-          )}
-
-          <View className='mt-8'>
-            <View>
               <Pressable
-                className='bg-blue-600 py-3 rounded-lg'
-                onPress={handleSubmit(handleLogin)}
+                onPress={() => setPasswordVisible((prevValue) => !prevValue)}
+                className='absolute right-0 mr-2'
               >
-                <Text className='text-white text-center font-semibold text-lg'>
-                  Sign Up
-                </Text>
+                <Ionicons
+                  name={passwordVisible ? "eye-off-outline" : "eye-outline"}
+                  size={20}
+                  color='gray'
+                />
               </Pressable>
             </View>
+          )}
+          name='password'
+        />
+        {errors.password && (
+          <Text className='text-red-500'>{errors.password.message}</Text>
+        )}
 
-            <View className='flex-row justify-center mt-5'>
-              <Text>Don't have an account?</Text>
-              <Text
-                onPress={() => router.push("/signup")}
-                className='ml-1 text-blue-600'
-              >
-                Sign up
+        <View className='mt-8'>
+          <View>
+            <Pressable
+              className='bg-blue-600 py-3 rounded-lg'
+              onPress={handleSubmit(handleSignIn)}
+            >
+              <Text className='text-white text-center font-semibold text-lg'>
+                Continue
               </Text>
-            </View>
+            </Pressable>
+          </View>
+
+          <View className='flex-row justify-center mt-5'>
+            <Text>Dont have an account?</Text>
+            <Text
+              onPress={() => router.push("/signup")}
+              className='ml-1 text-blue-600'
+            >
+              Sign up
+            </Text>
           </View>
         </View>
-      </SafeAreaView>
-    );
-  };
+      </View>
+    </SafeAreaView>
+  );
 }
