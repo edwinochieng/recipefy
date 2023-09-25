721ed29d94f316066e3958e118b51952048fe1df
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-09-25 22:21:46 +0300

    edit stack layout

--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -1,12 +1,28 @@
 import { Redirect, Stack } from "expo-router";
 import { UserAuth } from "../../auth/AuthContext";
+import BackButton from "../../components/BackButton";
 
 export default function AppLayout() {
   const { user } = UserAuth();
 
-  if (!user) {
-    return <Redirect href='/login' />;
-  }
-
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name='(home)' options={{ headerShown: false }} />
+      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      <Stack.Screen
+        name='categories/[slug]'
+        options={{
+          headerLeft: () => <BackButton />,
+          headerTitle: "",
+        }}
+      />
+      <Stack.Screen
+        name='details/[id]'
+        options={{
+          headerLeft: () => <BackButton />,
+          headerTitle: "",
+        }}
+      />
+    </Stack>
+  );
 }
