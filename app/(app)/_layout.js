f66debc3e020dc9e8c8896076edc955a55482727
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-11-17 19:54:44 +0300

    remove auth screens

--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -1,14 +1,10 @@
-import { Redirect, Stack } from "expo-router";
-import { UserAuth } from "../../auth/AuthContext";
+import { Stack } from "expo-router";
 import BackButton from "../../components/BackButton";
 
 export default function AppLayout() {
-  const { user } = UserAuth();
-
   return (
     <Stack>
       <Stack.Screen name='(home)' options={{ headerShown: false }} />
-      <Stack.Screen name='(auth)' options={{ headerShown: false }} />
       <Stack.Screen
         name='categories/[slug]'
         options={{
