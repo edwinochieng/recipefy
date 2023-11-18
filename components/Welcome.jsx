d7cbe4b9d77d3a2ea94440a6119bb8870646f7ea
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-11-18 19:30:10 +0300

    remove user name

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -4,9 +4,6 @@ import React from "react";
 export default function Welcome() {
   return (
     <View className='px-4'>
-      <Text className='text-gray-500 font-poppins-medium text-base mb-1'>
-        Hello Edwin
-      </Text>
       <Text className='text-3xl font-poppins-bold text-gray-700 '>
         What would you like {"\n"} to cook today?
       </Text>
