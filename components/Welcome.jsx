4eac00d8bbc5a33aba2c5ad786a6610e2709bebe
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-11-18 19:32:06 +0300

    add vertical margins

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Welcome() {
   return (
-    <View className='px-4'>
+    <View className='px-4 mt-4 mb-2'>
       <Text className='text-3xl font-poppins-bold text-gray-700 '>
         What would you like {"\n"} to cook today?
       </Text>
